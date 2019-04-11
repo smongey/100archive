f56a6edbf1f4fee399e1c76b1f8e6c56280a89a8
naoiseo <naoise@detail.ie>
2019-04-11 11:50:56 +0100

    changed homepage

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
     <section class="intro">
         <div class="intro_title">
-            <h2>The 100 Archive charts the past, present and future of Irish design by publishing 100 notable communication design projects, selected each year following an&nbsp;open&nbsp;call.</h2>
+            <h2>The 200 Archive charts the past, present and future of Irish design by publishing 100 notable communication design projects, selected each year following an&nbsp;open&nbsp;call.</h2>
         </div>
         <div class="stack intro_stack">
             <ul>
