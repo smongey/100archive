b5a9537ef99df790d1b513c674efe6a89b8dd265
naoiseo <naoise@detail.ie>
2019-04-11 12:15:41 +0100

    whatever

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 <div class="wrap">
 
+    
     <section class="intro">
         <div class="intro_title">
             <h2>The 6000 Archive charts the past, present and future of Irish design by publishing 100 notable communication design projects, selected each year following an&nbsp;open&nbsp;call.</h2>
